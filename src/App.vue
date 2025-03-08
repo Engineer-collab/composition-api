039bad0a4ead715d7839071f615e78a24f788b13
Waqas Tahir <wqs.wtu@gmail.com>
2025-03-08 23:26:01 +0500

    almost done

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,66 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <user-data :age="userAge" :firstName="firstName" :lastName="lastName"></user-data>
+    <!-- <h2>{{ userName }}</h2>
+    <h3>{{ userAge }}</h3> -->
+    <button @click="setAge">Change Age</button>
+    <div>
+      
+    <input type="text" placeholder="first name" v-model="firstName">
+    <input type="text" placeholder="last name" ref="lastNameInput">
+    <button @click="setLastName">Set Last Name</button>
+    </div>
   </section>
+  
+  
 </template>
 
 <script>
 
-import { ref  } from "vue";
+
+import { ref,computed,watch  } from "vue";
+import userData from "./components/userData.vue";
 export default {
-  setup(){
-    const Uname = ref('waqas wattu')
-    setTimeout(function(){
-      Uname.value = 'engineer waqas'
-    },2000)
-    return { userName : Uname}
+  components:{
+    userData
+  },
+setup(){
+//  const uName = ref('waqas wattu'); 
+ const uAge = ref(31);
+ const firstName = ref('');
+ const lastName = ref('');
+ const lastNameInput= ref(null)
+  // const user = reactive({
+  //   name : 'waqas wtu',
+  //   age : 31
+  // });
+  function setNewAge(){
+    uAge.value = 32
+  } 
+  function setLastName(){
+  lastName.value = lastNameInput.value.value;
   }
+  const uName = computed(function(){
+  return firstName.value + ' ' + lastName.value;
+  });
+  watch([uAge,uName],function(newValue,oldValue){
+  console.log('old age',oldValue[0])
+  console.log('new age',newValue[0])
+
+  console.log('old name',oldValue[1])
+  console.log('new name',newValue[1])
+  })
+// function setfirstName(event){
+//   firstName.value = event.target.value;
+// }
+// function setlastName(event){
+//   lastName.value = event.target.value;
+// }
+ 
+  return{
+    userName:uName,userAge:uAge,setAge:setNewAge,lastName,firstName:firstName,setLastName,lastNameInput
+  }
+}
   // data() {
   //   return {
   //     userName: 'Maximilian',
